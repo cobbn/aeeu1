d813809d4adb7000ee9040cd23a3e4002c1ed639
cobbn <168705970+cobbn@users.noreply.github.com>
2025-02-19 12:00:32 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
@@ -15,7 +15,6 @@ from requests import RequestException, Session, get, post
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-from bot.core.config_manager import Config
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from bot.helper.ext_utils.help_messages import PASSWORD_ERROR_MESSAGE
 from bot.helper.ext_utils.links_utils import is_share_link
